912252da2833462c191d5df7d20ec036d185a768
nonzamo <nonzamomwende57@gmail.com>
2024-07-06 11:13:30 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+// WEKEEND LAB
+let saturdayFun = "roller-skate";
+const fn = function(){
+    console.log (`${saturdayFun}`,);
+};
+fn();
+//function has been epressed using a function expression
+
+let mondayWork = "go to the office";
+const f = function(){
+    console.log (`${mondayWork}`,);
+};
+f();
+//function has been epressed using a function expression
+
+function wrapAdjective(name="Amali", describe ="beautiful"){
+ const innerfunction = function (special="charismatic"){
+        return (`${name}, ${describe}, ${special},`);
+};
+return innerfunction;
+} 
+wrapAdjective("Amali","Beautiful","charismatic");
+
+
+
+
+
